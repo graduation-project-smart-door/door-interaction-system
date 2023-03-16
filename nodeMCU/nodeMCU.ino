70745d0d52d015c7e08a50f5eaaf48031d36d863
Skaletti <gordey.korolev.2016@mail.ru>
2023-03-16 15:54:07 +0700

    servo add

--- nodeMCU/nodeMCU.ino
+++ nodeMCU/nodeMCU.ino
@@ -1,15 +1,21 @@
 #include <ESP8266WiFi.h> 
 #include <ESP8266WebServer.h>
+#include <Servo.h>
 
-const char* ssid = "RT-GPON-7D0D";
-const char* password = "p6keKG5P";
+Servo door_servo;
 
+#define MAX_SERVO 160
+#define DEFAULT_SERVO 0
+const char* ssid = "DIR-2150-C2AE";
+const char* password = "34583500";
 ESP8266WebServer server(80);   
 
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(9600);  
   delay(100);
+
+  door_servo.attach(5); //подключение сервопривода
 
   Serial.println("Connecting to ");
   Serial.println(ssid);
@@ -32,35 +38,47 @@ void setup()
   server.on("/test", handleGenericArgs1);  // привязать функцию обработчика к URL-пути
   server.on("/healthcheck", handleHealthcheck);  // привязать функцию обработчика к URL-пути
 
-  server.enableCORS(true);
+//  server.enableCORS(true);
+  server.sendHeader("Access-Control-Allow-Origin","*");
   server.begin();                                // запуск сервера
-  Serial.println("HTTP server started");  
-
+  Serial.println("HTTP server started"); 
 }
 
 void loop() 
 {
   server.handleClient();    // обработка входящих запросов
-
 }
 
-void doSometing() {
-  Serial.println("I`m working now");
+void doorOpen() {
+  Serial.println("Open process...");
+  int servo_now = DEFAULT_SERVO;
+  while(servo_now < MAX_SERVO) {
+    servo_now++;
+    Serial.println(servo_now);
+    door_servo.write(servo_now);
+    delay(15);
+  }
+  Serial.println("Close process...");
+  delay(3000);  
+  if(servo_now >= 160) {
+    while(servo_now >= DEFAULT_SERVO) {
+    servo_now--;
+    Serial.println(servo_now);
+    door_servo.write(servo_now);
+    delay(15);
+  }
+  }
+  Serial.println("Door close.");
 }
 
 void handleGenericArgs1() //обработчик
 {
-  doSometing();
-  
-  //...
-
+  doorOpen();
   server.send(200, "text/plain", "Hello World");    // ответить на HTTP запрос
 }
 
 void handleHealthcheck() //обработчик
 {
-  doSometing();
-  
   //...
 
   server.send(200, "text/plain", "Hello World");    // ответить на HTTP запрос
